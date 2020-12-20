5b50e4d240072e5fda04ba5fbc4fcf4c5a48400a
Elena Trajceva <elenatrajceva5@gmail.com>
2020-12-20 18:14:56 +0100

    Sepatate location

--- Домашна3/cafes.js
+++ Домашна3/cafes.js
@@ -1,15 +1,14 @@
+// initialize Leaflet
+var map = L.map('map').setView({lon: 21.4338, lat: 41.9987}, 15);
 
-    // initialize Leaflet
-    var map = L.map('map').setView({lon: 21.4338, lat: 41.9987}, 15);
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    //attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
 
-    // add the OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        //attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-    // show the scale bar on the lower left corner
-    L.control.scale().addTo(map);
+// show the scale bar on the lower left corner
+L.control.scale().addTo(map);
 
 
 /*getLocation();
@@ -25,43 +24,57 @@ function getLocation() {
         });
 }*/
 
-function showLocation(name) {
+function loadLocations(where) {
     $.ajax({
         url: 'kafe.csv',
         dataType: 'text',
-    }).done(find);
-    function find(data){
-        var allRows = data.split(/\r?\n|\r/);
-        for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
-            var rowCells = allRows[singleRow].split(',');
-                console.log(name.innerHTML);
-            if (rowCells[3] === name.innerHTML){
-                L.marker({lon: parseFloat(rowCells[2]), lat: parseFloat(rowCells[1])}).addTo(map);
+    }).done(
+        (data) => {
+            var allRows = data.split(/\r?\n|\r/);
+
+            for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
+                var rowCells = allRows[singleRow].split(',');
+                if(where !== rowCells[4]) continue;
+                console.log(rowCells);
+                L.marker([rowCells[1], rowCells[2]]).addTo(map)
+                    .bindPopup(`<p style="color:black;">${rowCells[3]}</p>`)
             }
-        }
-    }
+        });
 }
 
-parseData();
-function parseData() {
-    $.ajax({
-        url: 'kafe.csv',
-        dataType: 'text',
-    }).done(successFunction);
-}
-    function successFunction(data) {
-        var allRows = data.split(/\r?\n|\r/);
-        var table = '<table>';
-        for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
-            var rowCells = allRows[singleRow].split(',');
-            table += '<tr>';
-            table += '<td onclick="showLocation(this)">';
-            table += rowCells[3] + '</td>';
-            table += '<td>' + rowCells[4] + '<br>' + rowCells[5] + '<br>';
-            table += '</td>';
-            table += '</tr>';
-        }
-        table += '</tbody>';
-        table += '</table>';
-        $('body').append(table);
+loadLocations(findGetParameter('where'));
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
     }
+    return result;
+}
+// parseData();
+//
+// function parseData() {
+//     $.ajax({
+//         url: 'kafe.csv',
+//         dataType: 'text',
+//     }).done(successFunction);
+// }
+//
+// function successFunction(data) {
+//     var allRows = data.split(/\r?\n|\r/);
+//     var table = '<table>';
+//     for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
+//         var rowCells = allRows[singleRow].split(',');
+//         table += '<tr>';
+//         table += '<td onclick="showLocation(this)">';
+//         table += rowCells[3] + '</td>';
+//         table += '<td>' + rowCells[4] + '<br>' + rowCells[5] + '<br>';
+//         table += '</td>';
+//         table += '</tr>';
+//     }
+//     table += '</tbody>';
+//     table += '</table>';
+//     // $('body').append(table);
+// }
